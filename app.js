463ac7d4c39af5568915e4d997c13652aafb3917
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-01 21:46:02 -0500

    creating our own objects

--- app.js
+++ app.js
@@ -3,3 +3,21 @@
 ****************************************************/
 var tuna = "hey i am a tuna fish!";
 document.write(tuna.length);
+document.write("<br>");
+/****************************************************
+*              Creating Our Own Objects             *
+****************************************************/
+// constructor function
+function person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+// create a new instance of an Objects
+var bucky = new person("Bucky Roberts", 24);
+var taylor = new person("Taylor Hopkins", 20);
+document.write("<br>");
+
+document.write(bucky.name);
+document.write("<br>");
+document.write(taylor.age);
