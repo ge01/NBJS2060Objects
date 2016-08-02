9692d385913df2ccc0b000e51ecdc4e2744b0ac3
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-01 22:15:08 -0500

    adding methods to our objects

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 var tuna = "hey i am a tuna fish!";
 document.write(tuna.length);
 document.write("<br>");
+
 /****************************************************
 *              Creating Our Own Objects             *
 ****************************************************/
@@ -21,3 +22,33 @@ document.write("<br>");
 document.write(bucky.name);
 document.write("<br>");
 document.write(taylor.age);
+document.write("<br>");
+
+/****************************************************
+*                 Object Initializers               *
+****************************************************/
+plucky = {name:"Plucky Chicken", age:25};
+traylor = {name:"Traylor Shift", age:20};
+
+document.write(plucky.name + " loves " + traylor.name + " because she is " + traylor.age);
+document.write("<br>");
+
+/****************************************************
+*            Adding Methods to Our Objects          *
+****************************************************/
+function people(name1, age1) {
+  this.name1 = name1;
+  this.age1 = age1;
+  this.yearsTilRetire = yearsLeft;
+}
+
+function yearsLeft(){
+  var numYears = 65 - this.age1;
+  return numYears;
+}
+var natalie = new people("Natalia kukulska", 29);
+var vlada = new people("Vlada Roslyakova", 30);
+
+document.write(natalie.yearsTilRetire());
+document.write("<br>");
+document.write(vlada.yearsTilRetire());
